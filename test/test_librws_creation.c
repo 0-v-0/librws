ea07197e4a16111a6f74062beb4c0e9fc5f3c917
azure <sysu.zqlong@gmail.com>
2021-07-25 11:50:18 +0800

    Optimize testcase
    Signed-off-by: azure <sysu.zqlong@gmail.com>

--- test/test_librws_creation.c
+++ test/test_librws_creation.c
@@ -22,6 +22,8 @@
 
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include <assert.h>
 #include <string.h>
 
@@ -46,38 +48,39 @@ static rws_socket _socket = NULL;
 static void on_socket_received_text(rws_socket socket, const char * text, const unsigned int length, rws_bool is_finished) {
     char buff[8*1024];
     memcpy(buff, text, length);
-    buff[length] = 0;
-
-    printf("\nSocket text: %s", text);
+    buff[length] = '\0';
+    printf("\nSocket text: %s\n", buff);
 }
 
 static void on_socket_received_bin(rws_socket socket, const void * data, const unsigned int length, rws_bool is_finished) {
     char buff[8*1024];
     memcpy(buff, data, length);
     buff[length] = 0;
+    printf("\nSocket bin: <%s>\n", buff);
+}
 
-    printf("\nSocket bin: <%s>", buff);
+static void on_socket_recvd_pong(rws_socket socket){
+    printf("\nSocket pong\n");
 }
 
 static void on_socket_connected(rws_socket socket) {
-    const char * test_send_text =
-    "{\"version\":\"1.0\",\"supportedConnectionTypes\":[\"websocket\"],\"minimumVersion\":\"1.0\",\"channel\":\"/meta/handshake\"}";
-
-    printf("\nSocket connected");
-
-    rws_socket_send_text(socket, test_send_text);
+    printf("\nSocket connected\n");
 }
 
 static void on_socket_disconnected(rws_socket socket) {
     rws_error error = rws_socket_get_error(socket);
     if (error) {
-        printf("\nSocket disconnect with code, error: %i, %s",
+        printf("\nSocket disconnect with code, error: %i, %s\n",
               rws_error_get_code(error),
               rws_error_get_description(error));
+    } else {
+        printf("\nSocket disconnect\n");
     }
 }
 
 int main(int argc, char* argv[]) {
+    const char * test_send_text =
+        "{\"version\":\"1.0\",\"supportedConnectionTypes\":[\"websocket\"],\"minimumVersion\":\"1.0\",\"channel\":\"/meta/handshake\"}";
     _socket = rws_socket_create(); // create and store socket handle
     assert(_socket);
 
@@ -90,14 +93,21 @@ int main(int argc, char* argv[]) {
     rws_socket_set_on_connected(_socket, &on_socket_connected);
     rws_socket_set_on_received_text(_socket, &on_socket_received_text);
     rws_socket_set_on_received_bin(_socket, &on_socket_received_bin);
+    rws_socket_set_on_received_pong(_socket, &on_socket_recvd_pong);
 
-#if !defined(RWS_APPVEYOR_CI)
+//#if !defined(RWS_APPVEYOR_CI)
     // connection denied for client applications
     rws_socket_connect(_socket);
-#endif
+    usleep(1000*1000);
+//#endif
 
-    // main loop here
+    rws_socket_send_ping(_socket);
+    usleep(1000*1000);
 
+    rws_socket_send_text(_socket, test_send_text);
+    usleep(1000*1000);
+
+    rws_socket_disconnect_and_release(_socket);
     return 0;
 }
 
